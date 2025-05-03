f23d490b96a56d2c4a7c88297ffdcf177555dac2
macos <rifqiadeetya@gmail.com>
2025-05-04 01:50:50 +0700

    [FIX] countdown error

--- wedding_plan/static/src/js/wedding.js
+++ wedding_plan/static/src/js/wedding.js
@@ -124,39 +124,36 @@ $(document).ready(function ($) {
                 hour = minute * 60,
                 day = hour * 24;
 
-            let today = new Date(),
-                dd = String(today.getDate()).padStart(2, "0"),
-                mm = String(today.getMonth() + 1).padStart(2, "0"),
-                yyyy = today.getFullYear(),
-                nextYear = yyyy + 1,
-                dayMonth = "05/04/",
-                birthday = dayMonth + yyyy;
+            // Set target date to May 4, 2025 at 10:00 AM
+            const targetDate = new Date("May 4, 2025 10:00:00").getTime();
+            
+            const x = setInterval(function () {
+                const now = new Date().getTime(),
+                    distance = targetDate - now;
 
-            today = mm + "/" + dd + "/" + yyyy;
-            if (today > birthday) {
-                birthday = dayMonth + nextYear;
-            }
+                // Check if elements exist before trying to access them
+                const daysEl = document.getElementById("days");
+                const hoursEl = document.getElementById("hours");
+                const minutesEl = document.getElementById("minutes");
+                const secondsEl = document.getElementById("seconds");
+                const headlineEl = document.getElementById("headline");
+                const countdownEl = document.getElementById("countdown");
+                const contentEl = document.getElementById("content");
 
-            const countDown = new Date(birthday).getTime(),
-                x = setInterval(function () {
+                if (daysEl) daysEl.innerText = Math.floor(distance / (day));
+                if (hoursEl) hoursEl.innerText = Math.floor((distance % (day)) / (hour));
+                if (minutesEl) minutesEl.innerText = Math.floor((distance % (hour)) / (minute));
+                if (secondsEl) secondsEl.innerText = Math.floor((distance % (minute)) / second);
 
-                    const now = new Date().getTime(),
-                        distance = countDown - now;
-
-                    document.getElementById("days").innerText = Math.floor(distance / (day)),
-                        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-                    //do something later when date is reached
-                    if (distance < 0) {
-                        document.getElementById("headline").innerText = "It's my birthday!";
-                        document.getElementById("countdown").style.display = "none";
-                        document.getElementById("content").style.display = "block";
-                        clearInterval(x);
-                    }
-                    //seconds
-                }, 0)
+                //do something later when date is reached
+                if (distance < 0) {
+                    if (headlineEl) headlineEl.innerText = "It's my birthday!";
+                    if (countdownEl) countdownEl.style.display = "none";
+                    if (contentEl) contentEl.style.display = "block";
+                    clearInterval(x);
+                }
+                //seconds
+            }, 0)
         }
         setCountdown()
         setTimeout(hideLoadingContent, 2500);
